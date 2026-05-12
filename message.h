60a03a36762cc2c60341daec111f7a710acc043f
wwwatannnabe <kousuke555go@icloud.com>
2026-05-12 13:48:38 +0900

    Refactor message.h to define new packet structures

--- message.h
+++ message.h
@@ -1,14 +1,11 @@
-struct modify_state {
+struct packet_in{
     struct ofp_header header;
-    struct ofp_match match;
-    uint64_t cookie;
-    uint16_t command;
-    uint16_t idle_timeout; uint16_t hard_timeout; uint16_t priority;
-    uint32_t buffer_id; uint16_t out_port; uint16_t flags;
-};
-
-struct hello{
-    struct ofp_header header;
+    uint32_t buffer_id;
+    uint16_t total_len;
+    uint16_t in_port;
+    uint8_t reason;
+    uint8_t pad;
+    uint8_t data[0];
 };
 
 struct flow_removed{
@@ -26,4 +23,53 @@ struct flow_removed{
     uint64_t byte_count;
 };
 
+struct port_status{
+	struct ofp_header header;
+	uint8_t reason;
+	uint8_t pad[7];
+	struct ofp_phy_port desc;
+};
 
+struct error{
+    struct ofp_header header;
+    uint16_t type;
+    uint16_t code;
+    uint8_t data[0];
+};
+
+struct feature{
+   struct ofp_header header;
+    uint64_t datapath_id;
+    uint32_t n_buffers;
+    uint8_t n_tables;
+    uint8_t pad[3];
+    uint32_t capablilities;
+    uint32_t actions;
+    struct ofp_phy_port ports[0];
+};
+
+struct config{
+    struct ofp_header header;
+    uint16_t flags;
+    uint16_t miss_send_len;
+};
+
+struct modify_state {
+    struct ofp_header header;
+    struct ofp_match match;
+    uint64_t cookie;
+    uint16_t command;
+    uint16_t idle_timeout; uint16_t hard_timeout; uint16_t priority;
+    uint32_t buffer_id; uint16_t out_port; uint16_t flags;
+};
+
+struct read_state{
+    struct ofp_header header;
+    uint16_t type;
+    uint16_t flags;
+    uint8_t body[0];
+};
+
+struct hello{
+    struct ofp_header header;
+};
